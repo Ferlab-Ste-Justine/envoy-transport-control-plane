169ec8307614e994d95bb43129b4f62e0558ec1d
Eric Vallee <eric_vallee@webificservices.com>
2023-05-03 15:07:31 -0400

    feat: INFRA-340 Updated etcd-sdk dependency

--- parameters/parameters.go
+++ parameters/parameters.go
@@ -81,26 +81,26 @@ func (r *Retriever) setParamsVersion(params *Parameters, etcdVer int64) error {
 	return nil
 }
 
-func (r *Retriever) getPrefixNodeParams(ctx context.Context, prefix string) ([]NodeParameters, int64, error) {
+func (r *Retriever) getPrefixNodeParams(prefix string) ([]NodeParameters, int64, error) {
 	result := []NodeParameters{}
 
-	info, revision, err := r.Client.GetPrefix(ctx, prefix)
+	info, err := r.Client.GetPrefix(prefix)
 	if err != nil {
-		return result, revision, err
+		return result, info.Revision, err
 	}
 
-	for _, val := range info {
+	for _, val := range info.Keys {
 		nodeId := strings.TrimPrefix(val.Key, prefix)
 
 		var params Parameters
 		err = yaml.Unmarshal([]byte(val.Value), &params)
 		if err != nil {
-			return result, revision, err
+			return result, info.Revision, err
 		}
 
 		err = r.setParamsVersion(&params, val.Version)
 		if err != nil {
-			return result, revision, err
+			return result, info.Revision, err
 		}
 
 		r.Logger.Infof("[Etcd] Adding snapshot for node %s on boot", nodeId)
@@ -111,11 +111,15 @@ func (r *Retriever) getPrefixNodeParams(ctx context.Context, prefix string) ([]N
 		})
 	}
 
-	return result, revision, nil
+	return result, info.Revision, nil
 }
 
-func (r *Retriever) watchPrefixNodeParams(ctx context.Context, prefix string, revision int64, retrievalChan chan<- NodeParametersRetrieval) {
-	changesChan := r.Client.WatchPrefixChanges(ctx, prefix, revision, true)
+func (r *Retriever) watchPrefixNodeParams(prefix string, revision int64, retrievalChan chan<- NodeParametersRetrieval) {
+	changesChan := r.Client.Watch(prefix, client.WatchOptions{
+		Revision: revision,
+		IsPrefix: true,
+		TrimPrefix: true,
+	})
 
 	r.Logger.Infof("[Etcd] Started watching for parameters updates")
 	for change := range changesChan {
@@ -185,7 +189,7 @@ func (r *Retriever) RetrieveParameters(conf config.Config, log logger.Logger) (c
 
 		r.Client = cli
 
-		nodesParams, revision, nodeParamsErr := r.getPrefixNodeParams(ctx, conf.EtcdClient.Prefix)
+		nodesParams, revision, nodeParamsErr := r.getPrefixNodeParams(conf.EtcdClient.Prefix)
 		if nodeParamsErr != nil {
 			paramsChan <- NodeParametersRetrieval{NodeParameters: NodeParameters{}, Error: nodeParamsErr}
 			return
@@ -195,7 +199,7 @@ func (r *Retriever) RetrieveParameters(conf config.Config, log logger.Logger) (c
 			paramsChan <- NodeParametersRetrieval{NodeParameters: nodeParams, Error: nil}
 		}
 
-		r.watchPrefixNodeParams(ctx, conf.EtcdClient.Prefix, revision+1, paramsChan)
+		r.watchPrefixNodeParams(conf.EtcdClient.Prefix, revision+1, paramsChan)
 	}()
 
 	return paramsChan, cancel
