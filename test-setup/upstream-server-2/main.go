0729fcd252d95596bec7364d85195827d30fa5d4
Eric Vallee <eric_vallee@webificservices.com>
2023-05-04 14:13:53 -0400

    feat: INFRA-340 Added printing of host headers in test upstream servers to troubleshoot

--- test-setup/upstream-server-2/main.go
+++ test-setup/upstream-server-2/main.go
@@ -10,6 +10,7 @@ import (
 func server(server string, port int64) {
 	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
+		fmt.Printf("Host header is %s\n", c.Request.Header.Get("Host"))
 		c.JSON(http.StatusOK, gin.H{
 			"server": server,
 		})
